d1ab660574f2757a56babdf02b497c858f1cfe3b
PapaBiju <bijuakhil7@gmail.com>
2023-04-09 13:13:38 +0530

    login_ui

---
+++ login_ui/lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kBackgroundColor = Color.fromARGB(255, 71, 116, 104);
+const kPrimaryColor = Color.fromARGB(255, 0, 0, 0);
+const kSecondaryColor = Color(0xFF265DAB);
